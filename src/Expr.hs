6dffc6887cbb86c03efd18d92dd3d18b936604b0
Agustín Fernández Bergé <agustinfb93@gmail.com>
2025-01-07 00:08:51 -0300

    Useful funs

--- src/Expr.hs
+++ src/Expr.hs
@@ -5,6 +5,7 @@ module Expr where
 import Prelude hiding (const, exponent)
 
 import PExpr
+
 import Symplify
 
 import qualified Number as N
@@ -55,9 +56,13 @@ instance Num Expr where
               Add xs <- return $ p' + q'
               simplifySum xs
 
-    abs = undefined
+    abs x
+        | isTrue $ isNegative x = negate x
+        | isTrue $ isPositive x = x
+        | otherwise = fail "abs is undefined for this expression"
+    
     signum = undefined
-
+        
 instance Fractional Expr where
     fromRational = return . Number . fromRational
     p / q = do
@@ -69,13 +74,67 @@ instance Fractional Expr where
 makeFun :: (PExpr -> PExpr) -> Expr -> Expr
 makeFun f x = Fail $ f <$> unFail x 
 
+---
+operands :: PExpr -> [PExpr]
+operands (Add xs) = xs
+operands (Mul xs) = xs
+operands (Pow x y) = [x, y]
+operands  (Fun _ xs) = xs
+operands _ = []
+
+
+freeOf :: PExpr -> PExpr -> Bool
+freeOf u t
+    | u == t = False
+freeOf (Symbol _) _ = True
+freeOf (Number _) _ = True
+freeOf u t = all (freeOf t) $ operands u
+
+
+linearForm :: PExpr -> PExpr -> Fail (PExpr, PExpr)
+linearForm u x
+    | u == x = return (1, 0)
+    | notASymbol x = fail "x must be a symbol"
+        where
+            notASymbol (Symbol _) = False
+            notASymbol _ = True
+linearForm u@(Number _) _ = return (0, u)
+linearForm u@(Symbol _) _ = return (0, u)
+linearForm u@(Mul _) x
+    | freeOf u x = return (0, u)
+    | otherwise = do
+                    udivx <- (return u) / (return x)
+                    if freeOf udivx x
+                        then return (0, u)
+                        else fail "not a linear form"
+linearForm u@(Add []) _ = return (0, u)
+linearForm (Add (u:us)) x = do
+                                (a,b) <- linearForm u x
+                                (c,d) <- linearForm (Add us) x
+                                a' <- simplifySum [a, c]
+                                b' <- simplifySum [b, d]
+                                return (a', b')
+linearForm u x
+    | freeOf u x = return (0, u)
+    | otherwise = fail "not a linear form"
+
+---
+
+
 instance Floating Expr where
     pi = return Pi
     exp = makeFun Exp
     log = makeFun Log
 
-    sin 0 = 0
-    sin x = makeFun Sin x
+    sin x = do
+                (a,b) <- x >>= (`linearForm` Pi)
+                -- x = a * pi + b
+                case a of
+                    1 -> case b of
+                            Mul cs | foldl (\c -> (c /=) . (isTrue . isNegative)) False cs -> Sin <$> negate (return b)
+                            _ -> negate $ return $ Sin b
+                                    
+                    _ -> makeFun Sin x
 
     cos 0 = 1
     cos x = makeFun Cos x
@@ -106,6 +165,25 @@ csc = makeFun Csc
 
 cot :: Expr -> Expr
 cot = makeFun Cot
+
+---------
+
+extractTriBool :: Fail TriBool -> TriBool
+extractTriBool (Fail Nothing) = U
+extractTriBool (Fail (Just x)) = x
+
+instance Assumptions Expr where
+    isNegative = extractTriBool . fmap isNegative
+    isPositive = extractTriBool . fmap isPositive
+    isZero = extractTriBool . fmap isZero
+    isEven = extractTriBool . fmap isEven
+    isOdd = extractTriBool . fmap isOdd
+    isInteger = extractTriBool . fmap isInteger
+                        
+
+
+
+--------
 
 numerator :: PExpr -> Expr
 numerator (Number n) = fromInteger $ N.numerator n
---
+++ src/Simplification/Fu.hs
@@ -0,0 +1,1 @@
+module Simplification.Fu where
