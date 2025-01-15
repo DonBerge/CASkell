d0528517f4cf95ae53c59a0a717e1af6b7d665b7
Agustín Fernández Bergé <agustinfb93@gmail.com>
2025-01-15 15:50:43 -0300

    expand sin expr

--- src/Simplification/Algebraic.hs
+++ src/Simplification/Algebraic.hs
@@ -2,23 +2,24 @@ module Simplification.Algebraic (
     expand
 ) where
 
-import Expr
 import PExpr
 
 import Symplify
 
 import Math.Combinatorics.Exact.Binomial (choose)
 
-expand :: Expr -> Expr
+expand :: MonadFail m => m PExpr -> m PExpr
 expand x = x >>= expand'
 
-expand' :: PExpr -> Expr
-expand' (Add []) = 0
+expand' :: MonadFail m => PExpr -> m PExpr
+expand' (Add []) = return 0
 expand' (Add [x]) = expand' x >>= expandFraction
-expand' (Add (x:xs)) = expand' x + expand' (Add xs) >>= expandFraction
-                        
+expand' (Add (x:xs)) = do
+                        x' <- expand' x
+                        xs' <- expand' (Add xs)
+                        simplifySum [x',xs'] >>= expandFraction -- autosimplificacion puede generar terminos no expanadidos, por lo que hay que expnadir otra vez
 
-expand' (Mul []) = 1
+expand' (Mul []) = return 1
 expand' (Mul [x]) = expand' x >>= expandFraction
 expand' (Mul (x:xs)) = do
                         x' <- expand' x
@@ -44,33 +45,36 @@ expand' (Fun f xs) = (Fun f <$> mapM expand' xs) >>= expandFraction
 
 expand' u = expandFraction u
 
-expandFraction :: PExpr -> Expr
+expandFraction :: MonadFail m => PExpr -> m PExpr
 expandFraction x = do
+                    n <- numerator x
                     d <- denominator x
                     if d == 1
                         then return x
-                        else numerator x / expand' d
+                        else expand' d >>= simplifyDiv n  --numerator x / expand' d
 
-expandProduct :: PExpr -> PExpr -> Expr
-expandProduct (Add []) _ = 0
-expandProduct (Add (r:rs)) s = expandProduct r s + expandProduct (Add rs) s >>= expand' -- autosimplificacion puede generar terminos no expanadidos, por lo que hay que expnadir otra vez
+expandProduct :: MonadFail m => PExpr -> PExpr -> m PExpr
+expandProduct (Add []) _ = return 0
+expandProduct (Add (r:rs)) s = do
+                                r' <- expandProduct r s
+                                rs' <- expandProduct (Add rs) s
+                                simplifySum [r',rs'] >>= expand' -- autosimplificacion puede generar terminos no expanadidos, por lo que hay que expnadir otra vez
 expandProduct r s@(Add _) = expandProduct s r
 expandProduct r s = simplifyProduct [r,s] >>= expandFraction
 
 -- Se asume que n es no negativo
-expandPower :: PExpr -> Integer -> Expr
-expandPower _ 0 = 1
+expandPower :: MonadFail m => PExpr -> Integer -> m PExpr
+expandPower _ 0 = return 1
 expandPower u 1 = return u
-expandPower 0 _ = 0
-expandPower (Add []) _ = 0
-expandPower (Add [f]) n = fromInteger n >>= simplifyPow f >>= expand'
+expandPower 0 _ = return 0
+expandPower (Add []) _ = return 0
+expandPower (Add [f]) n = simplifyPow f (fromInteger n) >>= expand'
 expandPower (Add (f:rs)) n = sequence [ do
                                             cf' <- cf k
                                             cr' <- expandPower (Add rs) (n-k)
                                             expandProduct cf' cr'  | k <- [0..n] ] >>= simplifySum >>= expandFraction -- autosimplificacion puede generar terminos no expanadidos, por lo que hay que expnadir otra vez
     where
-        cf :: Integer -> Expr
         cf k = do
                 fk <- simplifyPow f (fromInteger k)
                 simplifyProduct [fromInteger (choose n k), fk]
-expandPower u n = fromInteger n >>= simplifyPow u >>= expandFraction
+expandPower u n = simplifyPow u (fromInteger n) >>= expandFraction
