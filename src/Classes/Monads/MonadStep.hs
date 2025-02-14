782fbd8ce579b5d8e22e3671020f5065f6a59fd6
Berge <agustinfb93@gmail.com>
2025-02-14 15:11:23 -0300

    Step

--- src/Classes/Monads/MonadStep.hs
+++ src/Classes/Monads/MonadStep.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module MonadStep where
 
 import Control.Monad
@@ -5,26 +6,20 @@ import Control.Monad
 import Control.Applicative
 
 import Classes.StepTree
+import Data.Bifunctor (Bifunctor(second))
 
 newtype StepT m a = StepT { runStepT :: m (StepTree, a) }
 
 instance Monad m => Functor (StepT m) where
-    fmap f (StepT m) = StepT $ do
-        (step, a) <- m
-        return (step, f a)
+    fmap f = StepT . fmap (second f) . runStepT
 
 instance Monad m => Applicative (StepT m) where
-    pure a = StepT $ return (E, a)
+    pure = StepT . return . (E,)
     (<*>) = ap
- 
-addChildren :: StepTree -> StepTree -> StepTree
-addChildren E t = t
-addChildren t E = t
-addChildren (Step n o r c) t = Step n o r (c ++ [t])
 
 instance Monad m => Monad (StepT m) where
     return = pure
     m >>= k = StepT $ do
-        (step, a) <- runStepT m
+        (_, a) <- runStepT m
         (step', b) <- runStepT (k a)
-        return (addChildren step' step, b)
+        return (step', b) 
