f2ae3ed388397e49b71cfd7de45ae74041a41c1f
Berge <agustinfb93@gmail.com>
2025-02-16 23:06:47 -0300

    Eval

--- src/Evaluate/Numeric.hs
+++ src/Evaluate/Numeric.hs
@@ -1,7 +1,65 @@
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
-module Evaluate.Numeric (
-    eval
-) where
+-- permite pasar funciones polimorficas como argumento
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Evaluate.Numeric
+  ( eval,
+  )
+where
+
+import Expr
+import qualified Number as N
+import Structure
+
+type Context = [(Expr, N.Number)]
+
+-- mkUnaryNumberOp :: (a -> N.Number) -> a -> Expr
+-- mkUnaryNumberOp f = construct . Number . f
+
+-- |
+--    @evalFloatingOp f x@ sirve para evaluar @f x@, si @x@ es un numero entonces devuelve el valor numerico aproximado,
+--    sino retorna la expresión simbolica.
+evalFloatingOp :: (forall a. (Floating a) => a -> a) -> Expr -> Expr
+evalFloatingOp f (structure -> Number n) = construct $ Number $ f n
+evalFloatingOp f x = f x
+
+removeElement :: (Eq a) => a -> [(a, b)] -> [(a, b)]
+removeElement _ [] = []
+removeElement x ((y, z) : xs)
+  | x == y = xs
+  | otherwise = (y, z) : removeElement x xs
+
+eval :: Context -> Expr -> Expr
+eval ctx u@(structure -> Symbol _) = maybe u (construct . Number) $ lookup u ctx
+eval ctx (structure -> Derivative u x) = construct $ Derivative (eval (removeElement x ctx) u) x -- no evaluar la variable de derivacion
+eval ctx (structure -> Integral u x) = construct $ Integral (eval (removeElement x ctx) u) x -- no evaluar la variable de integracion
+eval ctx (structure -> DefiniteIntegral u x a b) =
+  let u' = eval (removeElement x ctx) u
+      a' = eval ctx a
+      b' = eval ctx b
+   in construct $ DefiniteIntegral u' x a' b' -- no evaluar la variable de integracion
+eval ctx u = eval' $ mapStructure (eval ctx) u
+  where
+    -- evaluar funciones con numeros como argumento, ademas de potencias de numeros
+    eval' (structure -> Pow a b)
+      | Number n <- structure a, Number m <- structure b = construct $ Number $ n ** m -- potencias de numeros
+    eval' (structure -> Sin x) = evalFloatingOp sin x -- uno de los sin evalua a numeros y otro a expresiones
+    eval' (structure -> Cos x) = evalFloatingOp cos x
+    eval' (structure -> Tan x) = evalFloatingOp tan x
+    eval' (structure -> Asin x) = evalFloatingOp asin x
+    eval' (structure -> Acos x) = evalFloatingOp acos x
+    eval' (structure -> Atan x) = evalFloatingOp atan x
+    eval' (structure -> Sinh x) = evalFloatingOp sinh x
+    eval' (structure -> Cosh x) = evalFloatingOp cosh x
+    eval' (structure -> Tanh x) = evalFloatingOp tanh x
+    eval' (structure -> Asinh x) = evalFloatingOp asinh x
+    eval' (structure -> Acosh x) = evalFloatingOp acosh x
+    eval' (structure -> Atanh x) = evalFloatingOp atanh x
+    eval' (structure -> Exp x) = evalFloatingOp exp x
+    eval' (structure -> Log x) = evalFloatingOp log x
+    eval' v = v
+
+{-
 import PExpr
 import Classes.EvalSteps (EvalSteps)
 import Symplify (simplifySum, simplifyProduct, simplifyPow, simplifyFun)
@@ -68,3 +126,4 @@ eval u c = do
                                               v <- snd c
                                               return $ (s, v):cs'
                                 _ -> return cs'
+-}
