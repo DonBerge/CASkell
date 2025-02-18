6e50adec2dcbcd741a02888a7a63688a809075fd
Berge <agustinfb93@gmail.com>
2025-02-18 18:48:23 -0300

    Add monads assumptions

---
+++ src/Classes/Monads/MonadAssumptions.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+module Classes.Monads.MonadAssumptions where
+
+import TriBool
+import Control.Monad.Reader
+
+data AssumptionsEnviroment = AssumptionsEnviroment {
+    isPositive :: TriBool,
+    isNegative :: TriBool,
+    isZero :: TriBool,
+    isEven :: TriBool,
+    isOdd :: TriBool,
+    isInteger :: TriBool
+}
+
+emptyAssumptions :: AssumptionsEnviroment
+emptyAssumptions = AssumptionsEnviroment U U U U U U
+
+type AssumptionsT = ReaderT AssumptionsEnviroment
+
+instance Show (m a) => Show (AssumptionsT m a) where
+    show = show . runAssumptionsT
+
+runAssumptionsT :: AssumptionsT m a -> m a
+runAssumptionsT = flip runReaderT emptyAssumptions
