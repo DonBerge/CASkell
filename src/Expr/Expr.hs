2a1502d6e23b9dd0aba9ab175b4c1b575108d29e
Berge <agustinfb93@gmail.com>
2025-03-05 17:11:58 -0300

    mas docs

--- src/Expr/Expr.hs
+++ src/Expr/Expr.hs
@@ -6,8 +6,17 @@ module Expr (
     module Expr.PrettyPrint
 ) where
 
+-- | Definición del tipo 'Expr'
 import Expr.ExprType
+
+-- | PatternSynonyms y funciones para manipular la estructura de expresiones
 import Expr.Structure
+
+-- | Herramientas para manipular polinomios, como la división polinomica y el maximo común divisor
 import Expr.PolyTools
+
+-- | Parser de expresiones
 import Expr.Parser
+
+-- | PrettyPrint de expresiones
 import Expr.PrettyPrint
